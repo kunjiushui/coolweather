40f24445f1024e4556c0258a226fa1edc414fbee
CSN <117997626@qq.com>
2018-12-25 21:03:35 +0800

    Four commit.

--- app/src/main/java/com/coolweather/android/FirstActivity.java
+++ app/src/main/java/com/coolweather/android/FirstActivity.java
@@ -5,8 +5,9 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.List;
